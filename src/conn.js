fc9f4c783bb3dc4257e899bd1becdf2d13fa3b7f
ankitrajput9981 <ankitr9981@gmail.com>
2022-03-27 18:15:08 +0530

    ankit

--- src/conn.js
+++ src/conn.js
@@ -1,5 +1,7 @@
 const mongoose =  require("mongoose");
-mongoose.connect("mongodb://localhost:27017/portfolio",{
+mongoose.connect("mongodb+srv://Ankitr9981:Iiitg123@cluster0.ggmky.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
+    useNewUrlParser: true,
+    useUnifiedTopology:true
     
 }).then(()=>{
     console.log(`connetion successful`);
